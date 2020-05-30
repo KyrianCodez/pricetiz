dad56c4096536dfd5f650b6a8819e36d64c0f68f
kyle-belle <kyle@cenedex.com>
2020-05-30 15:21:56 -0400

    added config file to git ignore

--- includes/config.template.php
+++ includes/config.template.php
@@ -13,8 +13,8 @@
 |
 */
   define( 'DB_HOST', 'localhost' );          // Set database host
-  define( 'DB_USER', 'cenedex' );             // Set database user
-  define( 'DB_PASS', 'Exclusive_wheels23#' );             // Set database password
+  define( 'DB_USER', '' );             // Set database user
+  define( 'DB_PASS', '' );             // Set database password
   define( 'DB_NAME', 'oswa' );        // Set database name
 
 ?>
