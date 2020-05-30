fcdd8b7d1a6ff1e05b293b8a3114339a3f0a8c80
kyle-belle <kyle@cenedex.com>
2020-05-30 19:00:41 -0400

    add search to products page

--- product.php
+++ product.php
@@ -1,3 +1,5 @@
+
+
 <?php
   $page_title = 'All Product';
   require_once('includes/load.php');
@@ -5,7 +7,20 @@
    page_require_level(2);
   $products = join_product_table();
 ?>
-<?php include_once('layouts/header.php'); ?>
+
+<?php
+  define("SEARCH_FORM_NAME", "searchText");
+  
+  if($_POST){
+    $searchText = $_POST[SEARCH_FORM_NAME];
+    if(!empty($searchText)){
+      
+    }
+    echo json_encode($products);
+  }
+?>
+<?php if(!$_POST): ?>
+  <?php include_once('layouts/header.php'); ?>
   <div class="row">
      <div class="col-md-12">
        <?php echo display_msg($msg); ?>
@@ -13,9 +28,12 @@
     <div class="col-md-12">
       <div class="panel panel-default">
         <div class="panel-heading clearfix">
-         <div class="pull-right">
-           <a href="add_product.php" class="btn btn-primary">Add New</a>
-         </div>
+          <div class="header-product-search-container">
+            <input type="text" id="product-search-input" class="form-control header-product-search" placeholder="Search" />
+          </div>
+          <div class="pull-right">
+            <a href="add_product.php" class="btn btn-primary">Add New</a>
+          </div>
         </div>
         <div class="panel-body">
           <table class="table table-bordered">
@@ -39,7 +57,7 @@
                 <th class="text-center" style="width: 100px;"> Actions </th>
               </tr>
             </thead>
-            <tbody>
+            <tbody id="product-table-body">
               <?php foreach ($products as $product):?>
               <tr>
                 <td class="text-center"><?php echo count_id();?></td>
@@ -81,4 +99,141 @@
       </div>
     </div>
   </div>
+
+  <script type="text/javascript">
+
+    var productColumns = ["id", "subType", "name", "quantity", "buy_price", "sale_price", "media_id", "date", "description", "singleUnit", "itemLink", "city", "email", "phone", "zipcode", "freeShipping", "company", "website", "purchaseType", "categorie", "image"];
+
+    var tableColumns = ["#", "Photo", "ProductType", "Product Title", "Type", "SubType", "Instock", "Price", "Product Added", "ItemLink", "Company", "Website", "City", "ZipCode", "Phone", "Actions"];
+    
+    var tableProductColMap = new Map();
+    tableProductColMap.set("#", false);
+    tableProductColMap.set("Photo", "image");
+    tableProductColMap.set("ProductType", "purchaseType");
+    tableProductColMap.set("Product Title", "name");
+    tableProductColMap.set("Type", "categorie");
+    tableProductColMap.set("SubType", "subType");
+    tableProductColMap.set("Instock", "quantity");
+    tableProductColMap.set("Price", "sale_price");
+    tableProductColMap.set("Product Added", "date");
+    tableProductColMap.set("ItemLink", "itemLink");
+    tableProductColMap.set("Company", "company");
+    tableProductColMap.set("Website", "Website");
+    tableProductColMap.set("City", "city");
+    tableProductColMap.set("ZipCode", "zipcode");
+    tableProductColMap.set("Phone", "phone");
+    tableProductColMap.set("Actions", false);
+    
+    window.productColumns = productColumns;
+    
+    function generateTableData(products){
+      let productTableBody = document.getElementById("product-table-body");
+      if(productTableBody && Array.isArray(products)){
+        productTableBody.innerHTML = "";
+        let tableRows = ``;
+        products.forEach((p, index, arr) => {
+          console.log(p[productColumns.findIndex((c) => c === "name")]);
+
+          let row = `<tr>`;
+          
+          tableColumns.forEach((tCol, i) => {
+
+            const productCol = tableProductColMap.get(tCol);
+            console.log()
+            if(productCol){
+               if(productCol === "itemLink" && productCol === "website"){
+                  row += `<td><a target = '_blank' href="${p[productColumns.findIndex((c) => c === productCol)]}">Link</a></td>`;
+               }else if(productCol === "image"){
+                  let image_url = p[productColumns.findIndex((c) => c === "image")];
+
+                  if(image_url){
+                    row += `<td><img class="img-avatar img-circle" src="uploads/products/${image_url}" alt=""></td>`;
+                  }else{
+                    row += `<td><img class="img-avatar img-circle" src="uploads/products/no_image.jpg" alt=""></td>`;
+                  }
+               }else{
+                  row += `<td>${p[productColumns.findIndex((c) => c === productCol)] || ""}</td>`
+               }
+
+            }else if(tCol === "#"){
+              row += `<td>${index+1}</td>`
+            }else if(tCol === "Actions"){
+              row += `
+              <td><div class="btn-group">
+                    <a href="edit_product.php?id=${p[productColumns.findIndex((c) => c === "id")]}" class="btn btn-info btn-xs"  title="Edit" data-toggle="tooltip">
+                      <span class="glyphicon glyphicon-edit"></span>
+                    </a>
+                    <a href="delete_product.php?id=${p[productColumns.findIndex((c) => c === "id")]}" class="btn btn-danger btn-xs"  title="Delete" data-toggle="tooltip">
+                      <span class="glyphicon glyphicon-trash"></span>
+                    </a>
+              </div></td>`
+            }
+          });
+          
+          row += `</tr>`;
+          // console.log(`col`, col);
+          tableRows += row;
+        }, window);
+        productTableBody.innerHTML += tableRows;
+      }
+    }
+    
+    async function filterProduct(e){
+      let text = e.target.value;
+
+      var http = new XMLHttpRequest();
+      var data = "searchText=" + text;
+      
+      http.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          try{
+            let products = JSON.parse(this.responseText);
+
+            if(Array.isArray(products)){
+
+              products = products.filter((p) => {
+
+                try{
+                  const regex = new RegExp(`${text}`, "gi");
+                  for(let tCol of tableColumns){
+                  let pCol = tableProductColMap.get(tCol);
+                  if(pCol){
+                    console.log(p[productColumns.findIndex((c) => c === pCol)]);
+                    let matches = (p[productColumns.findIndex((c) => c === pCol)] || "").toString().match(regex);
+                    
+                    if(matches){
+                      return true;
+                    }
+                  }
+                }
+                }catch(e){
+                  console.log(e);
+                  return true;
+                }
+
+                
+
+                return false;
+              })
+              generateTableData(products);
+            }
+          }catch(e){
+            console.log(e)
+          }
+        }
+      }
+      console.log(window.location);
+      http.open('POST', window.location.href, true);
+      http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+      http.send(data);
+      
+    }
+
+    
+    var search_input = document.getElementById("product-search-input");
+    if(search_input){
+      search_input.addEventListener("input", filterProduct);
+    }
+  </script>
   <?php include_once('layouts/footer.php'); ?>
+  <?php endif ?>
